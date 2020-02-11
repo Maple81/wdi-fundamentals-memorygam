07ba6b1122a9cdb6b7f731996c7692d782a6efcf
Maple81 <stevenhaines06@gmail.com>
2020-02-11 20:48:17 +0900

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -22,25 +22,36 @@ cardImage: "images/king-of-diamonds.png"
 ];
 const cardsInPlay = [];
 
-function checkForMatch() {
+function checkForMatch(cardId) {
+	this.setAttribute("src",cards[cardId].cardImage);
 	if (cardsInPlay.length === 2) {	
 		if (cardsInPlay[0] === cardsInPlay[1]) {
 		  console.log("You found a match!");
-		
 		} else {
 		  console.log("Sorry, try again.");
 }
 }
 }
 
-function flipCard(cardId) {
-
+function flipCard() {
+	let cardId = this.getAttribute('data-id');  
+	cardElement.setAttribute('src',cardImage);
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
-	checkForMatch();
+	checkForMatch(cardId);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 };
 
-flipCard(0);
-flipCard(2);
+function createBoard() {
+	for (let i = 0; i < cards.length; i++) {
+		let cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById("game-board").appendChild(cardElement);
+	}
+};
+
+
+createBoard();
